a0d9ed7d44b98868a321fd212ff26b3ce14bef28
JUrielCerecero <urielcerecero1994@gmail.com>
2024-01-15 16:38:27 -0600

    pipline carga 2021-2023

--- sources/YGC_pipline_reviews_2021_2023.py
+++ sources/YGC_pipline_reviews_2021_2023.py
@@ -21,7 +21,7 @@ def datos_nuevos_Google():
         data = response.json()
 
         # Imprime los datos (o haz lo que desees con ellos)
-        print(data)
+        
     else:
         print(f"Error en la solicitud: {response.status_code}, {response.text}")
 
@@ -60,7 +60,7 @@ def datos_nuevos_Google():
     nuevos_datosG['text'] = nuevos_datosG['text'].apply(limpiar_texto)
 
     #Leemos la tabla de businessId_placeID
-    businessId_gmapID=pd.read_csv(r"C:\Users\p2_ge\Documents\proyectos\proyecto final\FinalProject_YelpGoogleMaps\datasets\csv\businessId_gmapID.csv")
+    businessId_gmapID=pd.read_csv("businessId_gmapID.csv")
 
     # Reemplazar 'Sin Valores' en 'place_id' con el valor de 'business_id'
     businessId_gmapID['place_id'] = businessId_gmapID.apply(lambda row: row['business_id'] if row['place_id'] == 'Sin Valores' else row['place_id'], axis=1)
@@ -74,8 +74,7 @@ def datos_nuevos_Google():
     return nuevos_datosG
 def datos_nuevos_yelp():
     # URL de la API con el token
-    api_url = "https://api.apify.com/v2/actor-tasks/frombini~yelp-scrap-task/runs/last/dataset/items?token=apify_api_ZE7FMykxbpsHef9FAMxUqF9esYgIGK2LrElm&unwind=reviews"
-
+    api_url = 'https://api.apify.com/v2/datasets/kGaVaPMnzYF5o8CEF/items?token=apify_api_ZE7FMykxbpsHef9FAMxUqF9esYgIGK2LrElm&unwind=reviews'
     # Realiza la solicitud GET
     response = requests.get(api_url)
 
@@ -83,9 +82,6 @@ def datos_nuevos_yelp():
     if response.status_code == 200:
         # Parsea la respuesta JSON
         data = response.json()
-
-        # Imprime los datos
-        print(data)
     else:
         print(f"Error en la solicitud: {response.status_code}, {response.text}")
     df=pd.DataFrame(data)
@@ -95,15 +91,11 @@ def datos_nuevos_yelp():
     dfreviewsYelp=df
     #Obtenemos el año , mes y hora de la columna date
 
-    dfreviewsYelp['date'] = pd.to_datetime(dfreviewsYelp['date'], format='%Y-%m-%d %H:%M:%S')
+    dfreviewsYelp['date'] = pd.to_datetime(dfreviewsYelp['date'], format='%Y-%m-%d')
     dfreviewsYelp['month'] = dfreviewsYelp['date'].dt.month
     dfreviewsYelp['year'] = dfreviewsYelp['date'].dt.year
     dfreviewsYelp['hour'] = dfreviewsYelp['date'].dt.time
     dfreviewsYelp.drop(columns=['date'],inplace=True)
-
-    #Filtramos los años a partir del 2019
-
-    dfreviewsYelp = dfreviewsYelp[(dfreviewsYelp['year'] >= 2019) & (dfreviewsYelp['year'] <= 2021)]
 
     # Convertimos la columna texto a minuscula y quitamos los caracteres especiales
 
@@ -124,7 +116,7 @@ def datos_nuevos_yelp():
 
 
 def cliente_bigquery():
-    credentials_path=r'C:\Users\p2_ge\Documents\proyectos\proyecto final\credencial google cloud\service_account.json'
+    credentials_path='service_account.json'
     credentials =  service_account.Credentials.from_service_account_file(credentials_path, scopes = ['https://www.googleapis.com/auth/cloud-platform'])
     client = bigquery.Client(credentials=credentials, project=credentials.project_id)
     return client
@@ -154,21 +146,20 @@ def checar_filas(dataframe_etl,tabla):
             continue
         else:
             filassincargar.append(row)
-        df=pd.DataFrame(filassincargar)
-        if len(filassincargar)==0:
-            return 'datos ya registrados 2021-2023'
-        else:
-            writetobigquery(df,f'windy-tiger-410421.UltaBeautyReviews.{tabla}')
-            return 'carga de filas pendientes 2021-2023'
+    df=pd.DataFrame(filassincargar)
+    if len(filassincargar)==0:
+        return 'datos ya registrados 2022-2023'
+    else:
+        writetobigquery(df,f'windy-tiger-410421.UltaBeautyReviews.{tabla}')
+        return 'carga de filas pendientes 2022-2023'
 def validación(dataframe,tabla,logs):
     results=query_bigquery('COUNT(*)',tabla,dataframe)
     registros=next(results.result())[0]
-    largo_dataframe=len(dataframe)
     if registros==0:
         writetobigquery(dataframe,f'windy-tiger-410421.UltaBeautyReviews.{tabla}')
-        cargar_logs(logs,'carga datos 2021-2023','Google reviews')
+        logs=cargar_logs(logs,'carga datos 2022-2023','Google reviews')
     else:
-        cargar_logs(logs,checar_filas(dataframe,tabla),'Google reviews')
+        logs=cargar_logs(logs,checar_filas(dataframe,tabla),'Google reviews')
 def cargar_logs(df,descripcion,archivo):
     fecha = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     hora= datetime.datetime.now().strftime("%H:%M:%S")
@@ -191,12 +182,14 @@ def upload_to_gcs(bucket_name, source_file_path, destination_blob_name):
 
     print(f'Archivo {source_file_path} subido a {destination_blob_name} en el bucket {bucket_name}.')
 def run():
-    client = google.cloud.storage.Client.from_service_account_json('service_account.json')
+    client = storage.Client.from_service_account_json('service_account.json')
     blob=client.bucket('ultabeauty').blob('logs/logs_loads.csv')
     contenido = blob.download_as_text()
     logs=pd.read_csv(StringIO(contenido))
     df=datos_nuevos_Google()
+    print('Etl completo reviews Google')
     validación(df,'google_reviews_ulta_beauty',logs)
+    print('validación completa google reviews')
     dfquery=query_bigquery('*','google_reviews_ulta_beauty',df).result().to_dataframe()
     # Itera sobre cada fila de df2 y verifica si está contenida en df1
     filassincargar=0
@@ -206,11 +199,12 @@ def run():
             continue
         else:
             filassincargar+=1
-    if filassincargar>0:
-        print('filas caragadas correctamente')
-        cargar_logs(logs,'verificación de carga completa reviews google 2021-2023','yelp reviews')
+    if filassincargar==0:
+        print('filas cargadas correctamente')
+        logs=cargar_logs(logs,'verificación de carga completa reviews google 2022-2023','yelp reviews')
     else:
         print(f'filas pendientes de cargar{filassincargar}')
+        logs=cargar_logs(logs,'cargar las filas pendientes','google reviews')
     df=datos_nuevos_yelp()
     validación(df,'yelp_reviews_ulta_beauty',logs)
     dfquery=query_bigquery('*','yelp_reviews_ulta_beauty',df).result().to_dataframe()
@@ -223,10 +217,11 @@ def run():
         else:
             filassincargar+=1
     if filassincargar>0:
-        print('filas caragadas correctamente')
-        cargar_logs(logs,'verificación de carga completa reviews yelp 2021-2023','yelp reviews')
+        print('filas cargadas correctamente')
+        logs=cargar_logs(logs,'verificación de carga completa reviews yelp 2021-2023','yelp reviews')
     else:
         print(f'filas pendientes de cargar{filassincargar}')
+        logs=cargar_logs(logs,'cargar las filas pendientes','yelp reviews')
     try:
 
         logs.to_csv('logs_loads.csv',index=False)
