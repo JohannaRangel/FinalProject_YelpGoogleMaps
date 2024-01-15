5dbd24d0b90e5aaad341a13384a6473be39d2996
Karen Sánchez <kareng.sanchezm@gmail.com>
2024-01-15 11:22:16 -0600

    updtae reading from bigQuery

--- sources/sentiment_analysis.py
+++ sources/sentiment_analysis.py
@@ -14,27 +14,32 @@ dataset_id = 'UltaBeautyReviews'
 table_idG = 'google_reviews_ulta_beauty'
 table_idY = 'yelp_reviews_ulta_beauty'
 
-# Construye y ejecuta la consulta para Google Reviews
+# Función para convertir una fila de BigQuery a un DataFrame de Pandas
+def convertir_a_dataframe(row):
+    return row[0]
+
+# Realiza la consulta a BigQuery para Google Reviews
 queryG = f"SELECT * FROM `{project_id}.{dataset_id}.{table_idG}` LIMIT 5"
 query_jobG = client.query(queryG)
 G_ulta_beauty = query_jobG.result()
 
-# Convierte el resultado a un DataFrame de Pandas
-G_ulta_beauty = pd.DataFrame(G_ulta_beauty)
+# Convierte los resultados a un DataFrame de Pandas
+df_G_ulta_beauty = pd.DataFrame([convertir_a_dataframe(row) for row in G_ulta_beauty], columns=list(G_ulta_beauty.schema.keys()))
 
-# Construye y ejecuta la consulta para Yelp Reviews
+# Repite el proceso para Yelp Reviews
 queryY = f"SELECT * FROM `{project_id}.{dataset_id}.{table_idY}` LIMIT 5"
 query_jobY = client.query(queryY)
 Y_ulta_beauty = query_jobY.result()
+df_Y_ulta_beauty = pd.DataFrame([convertir_a_dataframe(row) for row in Y_ulta_beauty], columns=list(Y_ulta_beauty.schema.keys()))
 
-# Convierte el resultado a un DataFrame de Pandas
-Y_ulta_beauty = pd.DataFrame(Y_ulta_beauty)
+# Concatena los DataFrames si es necesario
+ulta_beauty = pd.concat([df_G_ulta_beauty, df_Y_ulta_beauty], ignore_index=True)
 
+# Imprime el DataFrame resultante
+print(ulta_beauty.head())
 print('Los datos se cargaron correctamente')
 
 '''Correr el modelo'''
-
-ulta_beauty = pd.concat([G_ulta_beauty, Y_ulta_beauty])
 
 # Drop rows where 'text' is not a valid string
 ulta_beauty = ulta_beauty.dropna(subset=['text'])
