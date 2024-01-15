aa2c7ca7e33ce8ce4759884fc85b0a6e1c299224
Karen Sánchez <kareng.sanchezm@gmail.com>
2024-01-15 13:36:27 -0600

    update

--- sources/sentiment_analysis.py
+++ sources/sentiment_analysis.py
@@ -2,7 +2,6 @@ import pandas as pd
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 from google.cloud import bigquery
-import db-dtypes
 
 '''conexión a big query'''
 
