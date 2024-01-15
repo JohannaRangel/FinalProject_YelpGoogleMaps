b452662f69585047a1a11450265497548190d0c0
Karen Sánchez <kareng.sanchezm@gmail.com>
2024-01-15 11:39:57 -0600

    Update

--- sources/sentiment_analysis.py
+++ sources/sentiment_analysis.py
@@ -5,41 +5,46 @@ from google.cloud import bigquery
 
 '''conexión a big query'''
 
-# Configura tu proyecto y credenciales
-project_id = 'windy-tiger-410421'
-client = bigquery.Client(project=project_id)
-
-# Especifica tu conjunto de datos y tabla
-dataset_id = 'UltaBeautyReviews'
-table_idG = 'google_reviews_ulta_beauty'
-table_idY = 'yelp_reviews_ulta_beauty'
-
 # Función para convertir una fila de BigQuery a un DataFrame de Pandas
 def convertir_a_dataframe(row):
     return row[0]
 
-# Realiza la consulta a BigQuery para Google Reviews
-queryG = f"SELECT * FROM `{project_id}.{dataset_id}.{table_idG}` LIMIT 5"
-query_jobG = client.query(queryG)
-G_ulta_beauty = query_jobG.result()
+# Configura tu proyecto y credenciales
+project_id = 'windy-tiger-410421'
+client = bigquery.Client(project=project_id)
 
-# Convierte los resultados a un DataFrame de Pandas
-df_G_ulta_beauty = pd.DataFrame([convertir_a_dataframe(row) for row in G_ulta_beauty], columns=list(G_ulta_beauty.schema.keys()))
+# Especifica tu conjunto de datos y tabla para Yelp Reviews
+dataset_id = 'UltaBeautyReviews'
+table_idY = 'yelp_reviews_ulta_beauty'
+table_idG = 'yelp_reviews_ulta_beauty'
 
-# Repite el proceso para Yelp Reviews
+# Obtiene el esquema de la tabla de Yelp Reviews
+tableY = client.get_table(f'{project_id}.{dataset_id}.{table_idY}')
+tableG = client.get_table(f'{project_id}.{dataset_id}.{table_idG}')
+
+# Construye y ejecuta la consulta para Yelp Reviews
 queryY = f"SELECT * FROM `{project_id}.{dataset_id}.{table_idY}` LIMIT 5"
 query_jobY = client.query(queryY)
 Y_ulta_beauty = query_jobY.result()
-df_Y_ulta_beauty = pd.DataFrame([convertir_a_dataframe(row) for row in Y_ulta_beauty], columns=list(Y_ulta_beauty.schema.keys()))
+
+queryG = f"SELECT * FROM `{project_id}.{dataset_id}.{table_idY}` LIMIT 5"
+query_jobG = client.query(queryY)
+G_ulta_beauty = query_jobG.result()
+
+
+# Convierte los resultados a un DataFrame de Pandas
+df_Y_ulta_beauty = pd.DataFrame([convertir_a_dataframe(row) for row in Y_ulta_beauty], columns=[field.name for field in tableY.schema])
+df_G_ulta_beauty = pd.DataFrame([convertir_a_dataframe(row) for row in G_ulta_beauty], columns=[field.name for field in tableG.schema])
 
 # Concatena los DataFrames si es necesario
 ulta_beauty = pd.concat([df_G_ulta_beauty, df_Y_ulta_beauty], ignore_index=True)
 
 # Imprime el DataFrame resultante
 print(ulta_beauty.head())
+
 print('Los datos se cargaron correctamente')
 
-'''Correr el modelo'''
+'''Correr el modelo
 
 # Drop rows where 'text' is not a valid string
 ulta_beauty = ulta_beauty.dropna(subset=['text'])
@@ -62,8 +67,6 @@ ulta_beauty['sentiment'] = ulta_beauty['text'].apply(lambda x: analyze_sentiment
 print(ulta_beauty['sentiment'])
 print('El modelo se entreno correctamente')
 
-'''Cargar resultados
-
-
+Cargar resultados
 
 ulta_beauty.to_csv('..\datasets\csv\sentiment_analysis.csv')'''
