99132da37fac215bfb8fa1442592533bd820991a
JUrielCerecero <urielcerecero1994@gmail.com>
2024-01-18 22:24:32 -0600

    Update sentiment_analysis_carga_incremental_semanal.py

--- sources/sentiment_analysis_carga_incremental_semanal.py
+++ sources/sentiment_analysis_carga_incremental_semanal.py
@@ -2,7 +2,6 @@ import pandas as pd
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from google.cloud import bigquery
 from datetime import timedelta,datetime
-from google.oauth2 import service_account
 import torch
 from google.cloud import storage
 from io import StringIO
@@ -14,7 +13,7 @@ dataset_id = 'ultabeautyreviews'
 table_idY = 'yelp_reviews_ulta_beauty'
 table_idG = 'google_reviews_ulta_beauty'
 client = bigquery.Client()
-credentials =  service_account.Credentials.from_service_account_file('service_account.json')
+
 
 
 def analyze_sentiment(review):
@@ -35,7 +34,7 @@ def agregar_columnas_sentimientos(df):
 
 def writetobigquery(df,table_id):
     job_config0 = bigquery.LoadJobConfig(write_disposition = 'WRITE_APPEND',create_disposition= 'CREATE_IF_NEEDED')
-    client.load_table_from_dataframe(df, table_id, job_config=job_config0,)
+    client.load_table_from_dataframe(df,'final-project-data-insght-pro.ultabeautyreviews.'+table_id, job_config=job_config0,)
 
 def querys_bigquery(source):
 
@@ -44,21 +43,21 @@ def querys_bigquery(source):
     month_last_week=(datetime.now()-timedelta(8)).month
     month_today=datetime.now().month
     if year_today!=year_last_week:
-        query_sentiment_analisis_last_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_last_week} and month =12 and source={source}"
-        query_sentiment_analisis_current_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month =1 and source{source}"
-        query_reviews_last_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_last_week} and month =12 and source={source}"
-        query_reviews_current_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month =1 and source={source}"
+        query_sentiment_analisis_last_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_last_week} and month =12 and source='{source}'"
+        query_sentiment_analisis_current_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month =1 and source'{source}'"
+        query_reviews_last_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_last_week} and month =12 and source='{source}'"
+        query_reviews_current_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month =1 and source='{source}'"
         querys={'analisis sentimientos':[query_sentiment_analisis_last_year,query_sentiment_analisis_current_year],'reviews':[query_reviews_last_year,query_reviews_current_year]}
     if year_today==year_last_week:
         if month_today!=month_last_week:
-            query_sentiment_analisis_last_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month ={month_last_week} and source={source}"
-            query_sentiment_analisis_current_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month ={month_today} and source={source}"
-            query_reviews_last_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month ={month_last_week} and source={source}"
-            query_reviews_current_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month ={month_today} and source={source}"
+            query_sentiment_analisis_last_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month ={month_last_week} and source='{source}'"
+            query_sentiment_analisis_current_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month ={month_today} and source='{source}'"
+            query_reviews_last_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month ={month_last_week} and source='{source}'"
+            query_reviews_current_month=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month ={month_today} and source='{source}'"
             querys={'analisis_sentimientos':[query_sentiment_analisis_last_month,query_sentiment_analisis_current_month],'reviews':[query_reviews_last_month,query_reviews_current_month]}
         if month_today==month_last_week:
-            query_sentiment_analisis=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month ={month_today} and source={source}"
-            query_reviews=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month ={month_today} and source={source}"
+            query_sentiment_analisis=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month ={month_today} and source='{source}'"
+            query_reviews=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.google_reviews_ulta_beauty` where year={year_today} and month ={month_today} and source='{source}'"
             querys={'analisis_sentimientos':query_sentiment_analisis,'reviews':query_reviews}
     return querys
 def analisis(querys):
@@ -68,25 +67,25 @@ def analisis(querys):
         res_query_analisis_sentimientos=query_job.to_dataframe()
         query_job=client.query(querys['reviews'])
         res_querys_reviews=query_job.to_dataframe()
+        res_query_analisis_sentimientos.drop(columns=['sentiment_label','sentiment'],inplace=True)
     else:
+        df_sentiment=[]
+        df_reviews=[]
         for a in querys:
-            dfs_sentiment=[]
-            dfs_reviews=[]
             for b in querys[a]:
                 if a=='analisis_sentimientos':
                     query_job = client.query(querys['analisis_sentimientos'])
                     res_query_analisis_sentimientos=query_job.to_dataframe()
-                    dfs_sentiment.append(res_query_analisis_sentimientos)
+                    df_sentiment.append(res_query_analisis_sentimientos)
                 if a=='reviews':
                     query_job=client.query(querys['reviews'])
                     res_querys_reviews=query_job.to_dataframe()
-                    dfs_reviews.append(res_querys_reviews)
-    res_query_analisis_sentimientos=pd.concat(dfs_sentiment)
-    res_query_analisis_sentimientos.drop(columns=['sentiment_label','sentiment'],inplace=True)
-    del dfs_sentiment
-    res_query_analisis_sentimientos.drop(columns='')
-    res_querys_reviews=pd.concat(dfs_reviews)
-    del dfs_reviews
+                    df_reviews.append(res_querys_reviews)
+        res_query_analisis_sentimientos=pd.concat(df_sentiment)
+        res_query_analisis_sentimientos.drop(columns=['sentiment_label','sentiment'],inplace=True)
+        del df_sentiment
+        res_querys_reviews=pd.concat(df_reviews)
+        del df_reviews
     # Drop rows where 'text' is not a valid string
     res_querys_reviews.dropna(subset=['text'])
     res_querys_reviews= res_querys_reviews[res_querys_reviews['text'].apply(lambda x: isinstance(x, str))]
@@ -100,8 +99,12 @@ def analisis(querys):
                     df=agregar_columnas_sentimientos(fila)
                     writetobigquery(df,'ulta_beauty_sentiment_analysis')
                     filas_procesadas.append(df)
-    filas_procesadas=pd.concat(filas_procesadas)
-    return filas_procesadas
+    if len(filas_procesadas)==0:
+        return filas_procesadas
+    else:
+        filas_procesadas=[df for df in filas_procesadas if not df.empty]
+        filas_procesadas=pd.concat(filas_procesadas)
+        return filas_procesadas
 def cargar_logs(df,descripcion,archivo):
     fecha = datetime.now().strftime("%Y-%m-%d")
     hora= datetime.now().strftime("%H:%M:%S")
@@ -117,7 +120,7 @@ def comprobaciones(filas):
     month_today=datetime.now().month
     if year_today!=year_last_week:
         query_sentiment_analisis_last_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_last_week} and month =12"
-        query_sentiment_analisis_current_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month =1 and"
+        query_sentiment_analisis_current_year=f"SELECT * FROM `final-project-data-insght-pro.ultabeautyreviews.ulta_beauty_sentiment_analysis` where year={year_today} and month =1"
         querys=[query_sentiment_analisis_last_year,query_sentiment_analisis_current_year]
     if year_today==year_last_week:
         if month_today!=month_last_week:
@@ -133,7 +136,7 @@ def comprobaciones(filas):
         df=job.to_dataframe()
         df_bigquery.append(df)
     df_bigquery=pd.concat(df_bigquery)
-    client = storage.Client.from_service_account_json(credentials)
+    client = storage.Client()
     blob=client.bucket('ultabeauty2024').blob('logs/logs_loads.csv')
     contenido = blob.download_as_text()
     logs=pd.read_csv(StringIO(contenido))
@@ -158,10 +161,14 @@ def run():
     for fuente in ['G','Y']:
         querys=querys_bigquery(fuente)
         filas_procesadas=analisis(querys)
-        filas_a_comprobar.append(filas_procesadas)
-    filas_a_comprobar=pd.concat(filas_a_comprobar)
-    resultado=comprobaciones(filas_a_comprobar)
-    return resultado
+        if len(filas_procesadas)>0:
+            filas_a_comprobar.append(filas_procesadas)
+    if len(filas_a_comprobar)>0:
+        filas_a_comprobar=pd.concat(filas_a_comprobar)
+        resultado=comprobaciones(filas_a_comprobar)
+        return resultado
+    else:
+        return 'analisis de sentimiento completado'
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
